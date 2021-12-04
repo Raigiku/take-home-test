92e1bba921203497b02af4ee6adee03956f4038b
Raigiku <raigiku@hotmail.com>
2021-12-04 00:35:53 -0500

    Add class-transformer to parse strings in routes

--- take-home-web-api/src/main.ts
+++ take-home-web-api/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { HttpValidationExceptionFilter } from './presentation/core/filter/http-validation.exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new HttpValidationExceptionFilter());
 
   const config = new DocumentBuilder()
