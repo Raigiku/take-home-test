b5e333c33ed6128d2eeb9974fae2cf48378c17e7
Raigiku <raigiku@hotmail.com>
2021-12-05 03:14:05 -0500

    Add undefined checking for commit author prop

--- take-home-web-api/src/infrastructure/interactor-infra/github-commit/get-github-commits.interactor.infra.impl.ts
+++ take-home-web-api/src/infrastructure/interactor-infra/github-commit/get-github-commits.interactor.infra.impl.ts
@@ -28,21 +28,29 @@ export class GetGithubCommitsInteractorInfraImpl
       page,
       elementsPerPage,
     );
-    return apiCommits.map(
-      (ac) =>
-        new GithubCommit(
-          ac.commit.message,
-          ac.sha,
-          new Date(ac.commit.author.date),
-          new GithubCommitAccount(
-            ac.author.login,
-            ac.author.avatar_url,
-            ac.author.html_url,
-            accountName,
-            repositoryName,
-          ),
-          ac.html_url,
-        ),
-    );
+    return apiCommits.map((ac) => {
+      if (ac.author == null) console.dir(ac);
+      return new GithubCommit(
+        ac.commit.message,
+        ac.sha,
+        new Date(ac.commit.author.date),
+        ac.author === undefined
+          ? new GithubCommitAccount(
+              ac.commit.author.name,
+              ac.committer.avatar_url,
+              '',
+              accountName,
+              repositoryName,
+            )
+          : new GithubCommitAccount(
+              ac.author.login,
+              ac.author.avatar_url,
+              ac.author.html_url,
+              accountName,
+              repositoryName,
+            ),
+        ac.html_url,
+      );
+    });
   }
 }
